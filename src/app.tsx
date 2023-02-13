50592e60b7044dba27d7e3298a9d1a08e3f9f632
Joe Anderson <joe@mousetrapped.co.uk>
2023-02-13 14:05:12 +0000

    UI

--- src/app.tsx
+++ src/app.tsx
@@ -1,47 +1,48 @@
 import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
 import { invoke } from '@tauri-apps/api/tauri'
-import './app.css'
 
-export function App<FC>() {
-  const [greetMsg, setGreetMsg] = useState<string>('')
-  const [name, setName] = useState<string>('')
+export const App = () => {
+  const [host, setHost] = useState<string>('')
 
-  const greet = async () => {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke('greet', { name }))
+  const handleInput = (event: Event) => {
+    const target = event.target as HTMLInputElement
+    setHost(target.value)
+  }
+
+  const handleSubmit = (event: Event) => {
+    event.preventDefault()
+    invoke('begin_ping', { host }).then(console.log, console.error)
   }
 
   return (
-    <div class="container">
-      <h1>Welcome to Tauri!</h1>
-      <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
+    <form class="flex flex-col items-center" onSubmit={handleSubmit}>
+      <h1 class="text-4xl font-bold mb-8">Ping UI</h1>
 
-      <p>Click on the Tauri, Vite, and Preact logos to learn more.</p>
+      <label class="contents">
+        <div class="text-xl font-extralight mb-4">Enter an address to ping</div>
 
-      <div class="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
+        <div class="flex items-center gap-2">
+          <div class="rounded-lg inset-border">
+            <input
+              type="text"
+              class="rounded-lg bg-input shadow-inner text-center p-1 focus:bg-input-focus"
+              placeholder="0.0.0.0"
+              size="20"
+              value={host}
+              onInput={handleInput}
+            />
+          </div>
+
+          <button
+            type="submit"
+            class="aspect-square rounded-full bg-button p-2 hover:bg-button-hover transition-colors cursor-default text-ui-text"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
+            </svg>
           </button>
         </div>
-      </div>
-      <p>{greetMsg}</p>
-    </div>
+      </label>
+    </form>
   )
 }
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'window': 'rgb(30, 30, 30)',
+        'input': 'rgb(37, 37, 37)',
+        'input-focus': 'rgb(27, 27, 27)',
+        'button': 'rgb(41, 41, 41)',
+        'button-hover': 'rgb(50, 50, 50)',
+        'ui-text': 'rgb(123, 123, 123)',
+      },
+    },
+  },
+  plugins: [],
+}
